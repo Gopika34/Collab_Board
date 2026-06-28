9120d721e64297e231086ea753f4944f7e03fb02
Gopiks-s-34 <sivagopika34@gmail.com>
2026-06-28 20:20:08 +0530

    Setup mongoDB server

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB=async () => {
+    try{
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("MongoDB is connected!");
+    }
+    catch(err){
+        console.log('MongoDB connection failed:',err.message);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+
+dotenv.config();
+connectDb();
+
+const app= express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT;
+app.listen(port,()=>{
+    console.log(`Server is running at ${port}`);
+})
