1710a098a0de2c3fc93f3902f82f880ec29e4484
Gopiks-s-34 <sivagopika34@gmail.com>
2026-07-01 19:23:52 +0530

    Add card controller

---
+++ backend/controllers/CardController.js
@@ -0,0 +1,47 @@
+import {cardModel} from "../models/Card.js";
+
+export const createCard=async (req,res) => {
+    try{
+        const card= cardModel.create({
+            title: req.body.title,
+            description:req.body.description,
+            listId:req.body.listId,
+            order:req.body.order
+        });
+        return res.status(201).json(card);
+    }
+    catch(err){
+        return res.status(500).json({message:err.message});
+    }
+}
+export const getCardByID=async (req,res) => {
+    try{
+        const cards = cardModel.findById({listId:req.body.listId});
+        return res.json(cards);
+    }
+    catch(err){
+        return res.status(500).json({message:err.message});
+    }
+}
+export const updateCard=async (req,res) => {
+    try{
+        const card= cardModel.findByIdAndUpdate(
+            req.params.listId,
+            req.body,
+            {new:true}
+        );
+        return res.json(card);
+    }
+    catch(err){
+        return res.status(500).json({message:err.message});
+    }
+}
+export const deleteCard=async (req,res) => {
+    try{
+        const card= cardModel.findByIdAndUpdate(req.body.listId);
+        return res.json({message:"Card deleted!"});
+    }
+    catch(err){
+        return res.status(500).json({message:err.message});
+    }
+}
