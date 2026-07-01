2325ae95a6e752f859c9d6c63c038765018c1b25
Gopiks-s-34 <sivagopika34@gmail.com>
2026-07-01 19:37:32 +0530

    Updated auth middleware

--- backend/middleware/AuthMiddleware.js
+++ backend/middleware/AuthMiddleware.js
@@ -1,15 +1,11 @@
 import jwt from 'jsonwebtoken';
 
-export const AuthMiddleware=(req,res,next)=>{
+export const AuthMiddleware=async(req,res,next)=>{
     const token= req.headers.authorization?.split(' ')[1]
     if(!token) return res.status(401).json({message:"Token not found"});
 
     try{
         const verifiedToken= await jwt.verify(token,process.env.JWT_SECRET);
-        console.log(verifiedToken);
-        // middleware
-        console.log("MIDDLEWARE SECRET:", process.env.JWT_SECRET);
-        console.log("TOKEN ID:", verifiedToken.id);
         req.user=verifiedToken;
         next();
 
