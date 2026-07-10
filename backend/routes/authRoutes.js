1f4ea9368390f9daa86135a32c4511b5a9f1bad9
Gopiks-s-34 <sivagopika34@gmail.com>
2026-07-10 20:37:19 +0530

    Add rate limiter

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import {login,signup} from "../controllers/AuthController.js"
-
+import { authLimiter } from "../middleware/RateLimiter.js";
 const authRouter= Router();
 
-authRouter.post('/signup',signup);
-authRouter.post('/login',login);
+authRouter.post('/signup',authLimiter,signup);
+authRouter.post('/login',authLimiter,login);
 
 export default authRouter;
