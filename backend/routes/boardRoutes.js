0b36c5a1b3268ea4b88b0414d61bea8ccd667a50
Gopiks-s-34 <sivagopika34@gmail.com>
2026-07-10 20:52:12 +0530

    Updated board routes

--- backend/routes/boardRoutes.js
+++ backend/routes/boardRoutes.js
@@ -1,10 +1,13 @@
-import {createBoard,fetchBoard,getBoardById} from "../controllers/BoardController.js";
+import {createBoard,fetchBoard,getBoardById,updateBoard, deleteBoard} from "../controllers/BoardController.js";
 import {Router} from "express";
-import { verifyBoardAccess } from "../middleware/OwnershipMiddleware.js";
+import { verifyBoardAccess,verifyBoardOwnerAccess } from "../middleware/OwnershipMiddleware.js";
+
 const boardRoutes= Router();
 
 boardRoutes.get('/',fetchBoard);
 boardRoutes.post('/',createBoard);
 boardRoutes.get('/:id',verifyBoardAccess,getBoardById);
+boardRoutes.get('/:id',verifyBoardOwnerAccess,updateBoard);
+boardRoutes.get('/:id',verifyBoardOwnerAccess,deleteBoard);
 
 export default boardRoutes;
