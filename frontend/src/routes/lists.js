b8551d4b76de7e8cbc71ad9442119537020f2f3c
Gopiks-s-34 <sivagopika34@gmail.com>
2026-07-15 20:00:24 +0530

    feat: implement list API service

---
+++ frontend/src/routes/lists.js
@@ -0,0 +1,8 @@
+import api from "../api/axios.js";
+
+const url='/lists';
+
+export const getLists=(boardId)=> api.get(`${url}/${boardId}`);
+export const createList =(data)=> api.post(url,data);
+export const updateList =(id,data)=> api.patch(`${url}/${id}`,data);
+export const deleteList =(id)=> api.delete(`${url}/${id}`);
