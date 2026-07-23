8f1fe71c1d4d152419e938af8f11a776c5dc7204
Gopiks-s-34 <sivagopika34@gmail.com>
2026-07-23 20:00:03 +0530

    feat: implement authentication pages

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -32,25 +32,35 @@ const SignupPage = () => {
         }
         catch(err){
             setPassword('');
-            setError(err.response?.data?.message || "Signup failed");
+            setError(err.response?.data?.message || "⚠ Signup Failed");
         }
         finally{
             setIsLoading(false);
         }
     }
+
+    const inputStyle ="w-full border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-300";
     return (
         <div className='min-h-screen flex justify-center items-center bg-gray-100'>
             <form onSubmit={handleSignup}
-                className='bg-white p-5 rounded-lg shadow-md w-96 flex flex-col gap-4'
+                className='bg-white p-8 rounded-xl shadow-lg hover:shadow-xl transition-all w-full max-w-md flex flex-col gap-4'
             >
-                <h3 className='text-3xl font-bold text-center text-blue-400 mt-3'>Create Account</h3>
-                <input className='w-full border border-gray-300 rounded p-3 focus:outline-none focus:ring-2 focus:ring-blue-200' type="text" value={userName} onChange={(e)=>setUserName(e.target.value)} placeholder='Enter username'/>
-                <input className='w-full border border-gray-300 rounded p-3 focus:outline-none focus:ring-2 focus:ring-blue-200' type="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Enter email'/>
-                <input className='w-full border border-gray-300 rounded p-3 focus:outline-none focus:ring-2 focus:ring-blue-200' type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='Enter password'/>
-                <button className='bg-blue-600 text-white font-medium py-2 rounded hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed transition-colors'
-                type='submit' disabled={isLoading}>{isLoading ? "Loading..." : "Signup"}</button>
+                <h3 className='text-3xl font-bold text-center text-slate-900 mb-2'>Create Account</h3>
+                <p className='text-lg font-medium text-center text-slate-700 mb-2'>Manage boards, tasks and projects in one place.</p>
+                <label htmlFor="userName" className='text-sm font-medium'>Username</label>
+                <input className={inputStyle} id='userName' type="text" value={userName} onChange={(e)=>setUserName(e.target.value)} placeholder='Enter username'/>
+                <label htmlFor="email" className='text-sm font-medium'>Email</label>
+                <input className={inputStyle} id='email' type="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Enter email'/>
+                <label htmlFor="password" className='text-sm font-medium'>Password</label>
+                <input className={inputStyle} id='password' type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='Enter password'/>
+                <button className='bg-blue-600 text-white font-medium py-3 rounded-xl hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed transition-all duration-200'
+                type='submit' disabled={isLoading}>{isLoading ? "Loading..." : "Create Account"}</button>
                 <p className='text-sm text-gray-500 text-center'>Already have an account? <Link to='/login' className='text-blue-600 text-sm hover:underline'>Login</Link></p>
-                {error && <p className='text-red-600 text-sm text-center mt-2'>{error}</p>}
+                {error && 
+                <div className='bg-red-50 border border-red-200 rounded-xl p-3 flex justify-center items-center'>
+                    <p className='text-red-600 text-sm text-center'>{error}</p>
+                </div>
+                }
             </form>
         </div>
     )
